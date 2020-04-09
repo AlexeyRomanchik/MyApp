21727530718a7dfdacb35c5b59f53c5ca2e191eb
Romanchik Aleksey <romanchik_2014@inbox.ru>
2020-04-09 15:25:51 +0300

    Добавлен контроллер администратора

---
+++ WebApplication/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
