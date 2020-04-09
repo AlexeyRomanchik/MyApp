b35a39e98b537b1da13b585c514e1b863272efb1
Romanchik Aleksey <romanchik_2014@inbox.ru>
2020-04-09 14:24:00 +0300

    Add user registration

---
+++ WebApplication/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+using WebApplication.ViewModels;
+
+namespace WebApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var user = new User { Email = model.Email, UserName = model.Email };
+   
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ WebApplication/Data/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Models;
+
+namespace WebApplication.Data
+{
+    public sealed class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
